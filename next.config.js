69009f8e67b67cc59f5005d57973b332aabd2d71
geek-abyte <osisegabriel@gmail.com>
2025-12-24 06:39:09 +0100

    Add ESLint configuration to ignore errors during production builds and update service worker for cache management

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const withPWA = require("next-pwa")({
 
 const nextConfig = withPWA({
   reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
